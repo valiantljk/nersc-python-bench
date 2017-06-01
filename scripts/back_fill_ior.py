7c8408382829fbe799b2284c2af7c1b673d30193
valiantljk <valiantljk@gmail.com>
2017-06-01 16:31:32 -0700

    update back fill py

---
+++ scripts/back_fill_ior.py
@@ -0,0 +1,47 @@
+#/bin/bash 
+old_ior='/project/projectdirs/mpccc/fbench/IOR_CSCRATCH/IOR_HSW'
+import os,sys
+xdir_dir=[ os.path.join(old_ior,name) for name in os.listdir(old_ior) if os.path.isdir(os.path.join(old_ior, name)) ]
+ior_test=[ os.path.join(xdir_dir[0],name) for name in os.listdir(xdir_dir[0]) if name.endswith('IOR') and '_1000000_' in name]
+
+print len(ior_test)
+
+print ior_test[0]
+import re
+import time,datetime
+fileior=ior_test[0]
+def construct_sql(fileior):
+    file=open(fileior,'r')
+    row = file.readlines()
+    dateior_unix=[]
+    for line in row:
+        if re.search("Began",line):
+            temdateior=line.replace('Began: ',"")
+            temdateior=temdateior.replace('\n',"")
+            print temdateior
+            dateior_unix.append(int(time.mktime(datetime.datetime.strptime(temdateior, "%c").timetuple())))
+            print dateior_unix
+        if re.search('Max Write',line):
+            ior_write=(line.split(' ')[2])
+            print ior_write
+        if re.search('clients',line):
+            numtasks=line.replace(' ',"").split('=')[-1].split('(')[0]
+        if re.search('Max Read', line):
+            ior_read=(line.split(' ')[3])
+            print ior_read
+            break
+    bench_name_write=fileior.split('/')[7]+'_write'
+    bench_name_read=fileior.split('/')[7]+'_read'
+    jobid=fileior.split('_')[-1].split('.')[0]
+    hostname='cori'
+    cmd=[]
+    cmd.append('python report-ior.py insert %s %s %s %s %s %s'%(bench_name_write, dateior_unix[0], jobid, numtasks, hostname, ior_write))
+    cmd.append('python report-ior.py insert %s %s %s %s %s %s'%(bench_name_read, dateior_unix[1], jobid, numtasks, hostname, ior_read))
+    print cmd[0]
+    print cmd[1]
+    return cmd
+
+
+cmd=construct_sql(fileior)
+os.system(cmd[0])
+#%run {cmd[1]}
